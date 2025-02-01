19b15b8bd025ce85e3b466fce12f51f46be51076
abrahamvsdev <abrahamvsdev@gmail.com>
2025-02-02 00:09:31 +0100

    refactor: remove unused Main classes and rename to Manin for clarity

--- softlearning/src/main/java/com/example/softlearning/applicationcore/entity/employee/appservices/Main.java
+++
@@ -1,7 +0,0 @@
-package com.example.softlearning.applicationcore.entity.employee.appservices;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/example/softlearning/applicationcore/entity/employee/appservices/Manin.java
@@ -0,0 +1,7 @@
+// package com.example.softlearning.applicationcore.entity.employee.appservices;
+
+// public class Manin {
+//     public static void main(String[] args) {
+//         System.out.println("Hello world!");
+//     }
+// }
