bbfa04734f024ca7e00514f442c1ca750fe90201
abrahamvsdev <abrahamvsdev@gmail.com>
2025-02-09 23:01:23 +0100

    feat: update ClientServices interface to use 'dni' instead of 'id' for improved clarity

--- softlearning/src/main/java/com/example/softlearning/applicationcore/entity/client/appservices/ClientServices.java
+++ softlearning/src/main/java/com/example/softlearning/applicationcore/entity/client/appservices/ClientServices.java
@@ -7,8 +7,8 @@ import com.example.softlearning.applicationcore.entity.sharedkernel.model.except
 @Service
 //Trato como ID el DNI, que es la clave primaria de la tabla
 public interface ClientServices {
-    public String getByIdToJson (String id) throws ServiceException;
-    public String getByIdToXml (String id) throws ServiceException;
+    public String getByIdToJson (String dni) throws ServiceException;
+    public String getByIdToXml (String dni) throws ServiceException;
     public String addFromJson (String client) throws ServiceException;
     public String addFromXml (String client) throws ServiceException;
     public String updateOneFromJson(String client) throws ServiceException;
